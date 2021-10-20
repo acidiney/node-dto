becbfb47cdfef9b6446a87b8e27c310a1f6bd913
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2021-10-21 00:56:18 +0100

    feat: allow boolean type

--- src/utils/types.js
+++ src/utils/types.js
@@ -3,9 +3,11 @@
 const validateNumber = (value) => typeof value === 'number'
 const validateString = (value) => typeof value === 'string'
 const validateDate = (value) => value instanceof Date;
+const validateBoolean = (value) => typeof value === 'boolean'
 
 module.exports = {
     String: validateString,
     Number: validateNumber,
     Date: validateDate,
+    Boolean: validateBoolean
 }
