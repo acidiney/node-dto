46d46aa6ab5aa7124de09bf6d5c94812b46deb84
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2021-10-21 01:37:25 +0100

    fix: schema need to be an array of objects

--- src/index.js
+++ src/index.js
@@ -10,124 +10,131 @@ const ValidateException = require('./exceptions/ValidateException');
  * @returns {null|boolean|*}
  */
 function validateLine(entry, data) {
-    if (entry.required) {
-        if (AvailableTypes[entry.type](data) && typeof data !== 'undefined') {
-            return data
-        }
-
-        return true
+  if (entry.required) {
+    if (AvailableTypes[entry.type](data) && typeof data !== 'undefined') {
+      return data;
     }
 
-    if (typeof data === 'undefined')
-        return null
+    return false;
+  }
 
-    if (AvailableTypes[entry.type](data)) {
-        return data
-    }
+  if (typeof data === 'undefined') return null;
 
-    return true;
+  if (AvailableTypes[entry.type](data)) {
+    return data;
+  }
+
+  return false;
 }
 
-/**
- *
- * @Author Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
- *
- * @param {Object[]} params
- * @param {String} params.name
- * @param {AvailableTypes} params.type
- * @param {String} params.serialize
- * @param {Boolean} params.required
- * @returns {{validate: (function(Object): *)}}
- */
 module.exports = {
-    MakeDto: (params) => {
-        const availableTypes = Object.keys(AvailableTypes);
+  /**
+   *
+   * @Author Acidiney Dias <acidineydias@gmail.com>
+   *
+   * @param {Object[]} params
+   * @param {String} params.name
+   * @param {AvailableTypes} params.type
+   * @param {String} params.serialize
+   * @param {Boolean} params.required
+   * @returns {{exportUsingSQL: (function(*=): *[]),entries: (function(): *[]), export: ((function((Object|Object[])): ({}|{}[]))|*), validate: (function(Object): {})}}
+   * @constructor
+   */
+  MakeDto: (params) => {
+    if (!(params instanceof Array))
+      throw new Error('schema need to be an array');
 
-        const dto = params;
+    const availableTypes = Object.keys(AvailableTypes);
 
-        (() => {
-            const types = dto.map((dt) => dt.type);
+    const dto = params;
 
-            types.forEach((type) => {
-                if (!availableTypes.includes(type)) {
-                    throw new ValidateException(`${type} not recognized!`)
-                }
-            })
-        })()
+    (() => {
+      const types = dto.map((dt) => dt.type);
 
-
-        return {
-            /**
-             * @param {Object} input
-             * @returns {Object}
-             */
-            validate: (input) => {
-
-                const keys = Object.keys(input);
-                const validated = {};
-
-                for (const entry of dto) {
-                    for (const key of keys) {
-                        if (entry.name === key) {
-
-                            const response = validateLine(entry, input[key])
-
-                            if (typeof response === 'boolean')
-                                throw new ValidateException(`Field ${entry.name} with value ${input[key]}, is not valid!`)
-
-                            validated[entry.serialize] = response
-                        }
-                    }
-                }
-
-                return validated
-            },
-
-            /**
-             * @returns {String[]}
-             */
-            entries: () => dto.map(dt => dt.name),
-
-            /**
-             *
-             * @param {object|object[]} data
-             * @returns {{}|{}[]}
-             */
-            export: (data) => {
-
-                const convert = (entry) => {
-                    const keys = Object.keys(entry);
-                    const serialize = {};
-
-                    for (const key of keys) {
-                        const founded = dto.find((dt) => dt.serialize === key)
-                        serialize[founded.name] = entry[key]
-                    }
-
-                    return serialize;
-                }
-
-                if (data instanceof Array) {
-                    return data.map(convert)
-                }
-
-                return convert(data)
-            },
-
-            /**
-             *
-             * @param entity
-             * @returns {*[]}
-             */
-            exportUsingSQL: (entity = null) => {
-                const sql = [];
-
-                for (const dt of dto) {
-                    sql.push(`${entity ? `${entity}.${dt.serialize} as ${dt.name}` : `${dt.serialize} as ${dt.name}`}`)
-                }
-
-                return sql;
-            }
+      types.forEach((type) => {
+        if (!availableTypes.includes(type)) {
+          throw new ValidateException(`${type} not recognized!`);
         }
-    }
-}
+      });
+    })();
+
+    return {
+      /**
+       * @param {Object} input
+       * @returns {Object}
+       */
+      validate: (input) => {
+        const keys = Object.keys(input);
+        const validated = {};
+
+        for (const entry of dto) {
+          for (const key of keys) {
+            if (entry.name === key) {
+              const response = validateLine(entry, input[key]);
+
+              if (typeof response === 'boolean')
+                throw new ValidateException(
+                  `Field ${entry.name} with value ${input[key]}, is not typeof ${entry.type}!`
+                );
+
+              validated[entry.serialize] = response;
+            }
+          }
+        }
+
+        return validated;
+      },
+
+      /**
+       * @returns {String[]}
+       */
+      entries: () => dto.map((dt) => dt.name),
+
+      /**
+       *
+       * @param {object|object[]} data
+       * @returns {{}|{}[]}
+       */
+      export: (data) => {
+        const convert = (entry) => {
+          const keys = Object.keys(entry);
+          const serialize = {};
+
+          for (const key of keys) {
+            const founded = dto.find((dt) => dt.serialize === key);
+            serialize[founded.name] = entry[key];
+          }
+
+          return serialize;
+        };
+
+        if (data instanceof Array) {
+          return data.map(convert);
+        }
+
+        return convert(data);
+      },
+
+      /**
+       *
+       * @param entity
+       * @returns {*[]}
+       */
+      exportUsingSQL: (entity = null) => {
+        const sql = [];
+
+        for (const dt of dto) {
+          sql.push(
+            `${
+              entity
+                ? `${entity}.${dt.serialize} as ${dt.name}`
+                : `${dt.serialize} as ${dt.name}`
+            }`
+          );
+        }
+
+        return sql;
+      },
+    };
+  },
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,157 +1,171 @@
 'use strict';
 
-const { expect } = require('chai')
-const { MakeDto } = require('../src/index')
+const { expect } = require('chai');
+const { MakeDto } = require('../src/index');
 
 describe('nodejs-dto', () => {
-    it('should not accept valid type [Image]', () => {
-        expect(() => MakeDto([
-            {
-                name: 'testName',
-                serialize: 'name',
-                type: 'Image',
-                required: true
-            }
-        ])).to.throw()
-    })
+  it('shoud not accept nothing different of array to make schema', () => {
+    expect(() =>
+      MakeDto({
+        name: 'testName',
+        serialize: 'name',
+        type: 'Image',
+        required: true,
+      })
+    ).to.throw();
 
-    it('should throws error when required field is not filled', () => {
+    expect(() => MakeDto()).to.throw();
 
-        const dto = MakeDto([
-            {
-                name: 'age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            }
-        ])
+    expect(() => MakeDto('Trash')).to.throw();
+  });
 
-        expect(() => dto.validate({
-            age: null
-        })).to.throw()
-    })
+  it('should not accept valid type [Image]', () => {
+    expect(() =>
+      MakeDto([
+        {
+          name: 'testName',
+          serialize: 'name',
+          type: 'Image',
+          required: true,
+        },
+      ])
+    ).to.throw();
+  });
 
-    it('shoud throws an error when pass a different type of value', () => {
-        const dto = MakeDto([
-            {
-                name: 'Age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            }
-        ])
+  it('should throws error when required field is not filled', () => {
+    const dto = MakeDto([
+      {
+        name: 'age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+    ]);
 
-        const requestProps = {
-            Age: 'Acidiney'
-        }
+    expect(() =>
+      dto.validate({
+        age: null,
+      })
+    ).to.throw();
+  });
 
-        expect(() => dto.validate(requestProps)).to.throws()
-    })
+  it('shoud throws an error when pass a different type of value', () => {
+    const dto = MakeDto([
+      {
+        name: 'Age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+    ]);
 
-    it('should validate all entries and returns a object with serialized data', () => {
-        const dto = MakeDto([
-            {
-                name: 'Age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            }
-        ])
+    const requestProps = {
+      Age: 'Acidiney',
+    };
 
-        const requestProps = {
-            Age: 12
-        }
+    expect(() => dto.validate(requestProps)).to.throws();
+  });
 
-        const props = {
-            age: 12
-        }
+  it('should validate all entries and returns a object with serialized data', () => {
+    const dto = MakeDto([
+      {
+        name: 'Age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+    ]);
 
-        expect(dto.validate(requestProps)).to.deep.equal(props)
-    })
+    const requestProps = {
+      Age: 12,
+    };
 
-    it('should returns all entries', () => {
-        const dto = MakeDto([
-            {
-                name: 'Age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            },
-            {
-                name: 'Name',
-                serialize: 'name',
-                type: 'String',
-                required: false
-            }
-        ])
+    const props = {
+      age: 12,
+    };
 
+    expect(dto.validate(requestProps)).to.deep.equal(props);
+  });
 
-        expect(dto.entries()).to.deep.equal(['Age', 'Name'])
-    })
+  it('should returns all entries', () => {
+    const dto = MakeDto([
+      {
+        name: 'Age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+      {
+        name: 'Name',
+        serialize: 'name',
+        type: 'String',
+        required: false,
+      },
+    ]);
 
-    it('should get an model data from db to export', () => {
-        const dto = MakeDto([
-            {
-                name: 'Age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            },
-            {
-                name: 'Name',
-                serialize: 'name',
-                type: 'String',
-                required: false
-            }
-        ])
+    expect(dto.entries()).to.deep.equal(['Age', 'Name']);
+  });
 
-        expect(dto.export({ age: 12, name: 'Acidiney Dias' })).to.deep.equal({
-            Age: 12,
-            Name: 'Acidiney Dias'
-        })
-    })
+  it('should get an model data from db to export', () => {
+    const dto = MakeDto([
+      {
+        name: 'Age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+      {
+        name: 'Name',
+        serialize: 'name',
+        type: 'String',
+        required: false,
+      },
+    ]);
 
-    it('should export an sql without entity to runs on select', () => {
-        const dto = MakeDto([
-            {
-                name: 'Age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            },
-            {
-                name: 'Name',
-                serialize: 'name',
-                type: 'String',
-                required: false
-            }
-        ])
+    expect(dto.export({ age: 12, name: 'Acidiney Dias' })).to.deep.equal({
+      Age: 12,
+      Name: 'Acidiney Dias',
+    });
+  });
 
-        expect(dto.exportUsingSQL()).to.deep.equal([
-            'age as Age',
-            'name as Name'
-        ])
-    })
+  it('should export an sql without entity to runs on select', () => {
+    const dto = MakeDto([
+      {
+        name: 'Age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+      {
+        name: 'Name',
+        serialize: 'name',
+        type: 'String',
+        required: false,
+      },
+    ]);
 
-    it('should export an sql with entity to runs on select', () => {
-        const dto = MakeDto([
-            {
-                name: 'Age',
-                serialize: 'age',
-                type: 'Number',
-                required: true
-            },
-            {
-                name: 'Name',
-                serialize: 'name',
-                type: 'String',
-                required: false
-            }
-        ])
+    expect(dto.exportUsingSQL()).to.deep.equal(['age as Age', 'name as Name']);
+  });
 
-        expect(dto.exportUsingSQL('users')).to.deep.equal([
-            'users.age as Age',
-            'users.name as Name'
-        ])
-    })
-})
+  it('should export an sql with entity to runs on select', () => {
+    const dto = MakeDto([
+      {
+        name: 'Age',
+        serialize: 'age',
+        type: 'Number',
+        required: true,
+      },
+      {
+        name: 'Name',
+        serialize: 'name',
+        type: 'String',
+        required: false,
+      },
+    ]);
+
+    expect(dto.exportUsingSQL('users')).to.deep.equal([
+      'users.age as Age',
+      'users.name as Name',
+    ]);
+  });
+});
