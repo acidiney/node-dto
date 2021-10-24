83a17fbbe0ac5f5310027d4ab89519c8aeb4a814
Ajeya <ajeyabhat.off@gmail.com>
2021-10-25 01:05:55 +0530

    refactor: added nyc_output/ to .gitignore

---
+++ src/utils/validators/validateDefaultValue.js
@@ -0,0 +1,16 @@
+'use strict';
+const ValidateException = require('../../exceptions/ValidateException')
+module.exports = {
+    validateDefaultValue: (objDto, i)=>{
+        let defValType;
+        if (objDto.defaultValue !== null) {
+            defValType = (objDto.defaultValue instanceof Array) ? 'array' : typeof objDto.defaultValue;
+            if (defValType !== objDto.type.toLowerCase()) {
+                throw new ValidateException(
+                    `Value of the Field defaultValue is not of the same type 
+                      as the value given in field type on schema index ${i}!`
+                );
+            }
+        }
+    }
+}
